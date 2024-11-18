588b1a1d5099d72351db09d7bdfe652507fcee3a
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-11-18 17:24:14 +0100

    Refactoring config structure, loading all enviromental variables from a single file

--- engine/Configuration/config.py
+++ engine/Configuration/config.py
@@ -1,22 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 # Database configuration
-#app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'mysql') --dodavao mislio sam da ce pomoci, ali mrka kapa
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:ftn123@mysql:3306/social_network'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{os.getenv("MYSQL_USER")}:{os.getenv("MYSQL_PASSWORD")}@{os.getenv("MYSQL_HOST")}:{os.getenv("MYSQL_PORT")}/{os.getenv("MYSQL_DATABASE")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 CORS(app, supports_credentials=True)
 
 db = SQLAlchemy(app)
 
-from routes import test_bp, posts_bp
+# Registering blueprints after app and db are created to avoid cyclic imports
+from routes.post_routes import posts_bp
 
-app.register_blueprint(test_bp)
 app.register_blueprint(posts_bp)
 
 
