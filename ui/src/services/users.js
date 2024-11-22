06bd84c2b098e38f468d3bcbcc5ddaf96686908b
tomicdusann <148521690+tomicdusann@users.noreply.github.com>
2024-11-22 15:45:02 +0100

    Added few more admin routes and fixed code in controller, edited users service in UI app

--- ui/src/services/users.js
+++ ui/src/services/users.js
@@ -2,7 +2,25 @@ import axios from 'axios'
 
 export const fetchUsers = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/admin')
+    const response = await axios.get('http://localhost:5000/admin/allusers')
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const fetchBlockedUsers = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/admin/blockedusers')
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const unblockUser = async (userId) => {
+  try {
+    const response = await axios.post(`http://localhost:5000/admin/unblock/${userId}`)
     return response.data
   } catch (error) {
     console.error(error)
