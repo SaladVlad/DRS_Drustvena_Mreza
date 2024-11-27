d10d10e2a027d6557d419c5cd89612835c65781e
LukaSaric9 <lukisaric@gmail.com>
2024-11-27 14:26:20 +0100

    Adding profile and friends page

---
+++ ui/src/pages/friends.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UseState, useEffect} from 'react'
+
+const Friends = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Friends
---
+++ ui/src/pages/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UseState, useEffect} from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Profile
