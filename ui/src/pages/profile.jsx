5b785f79a9b28ddb9548b73fea1ed46af1900121
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-11-27 14:46:42 +0100

    importing navbar

--- ui/src/pages/profile.jsx
+++ ui/src/pages/profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { UseState, useEffect} from 'react'
+import { UseState, useEffect } from 'react'
+import NavBar from '../components/NavBar'
 
 const Profile = () => {
-    return (
-        <div>
-            <NavBar/>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
 }
 
 export default Profile
