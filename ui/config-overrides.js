8e3f94eeca56965bc37e49e6bf04807791ddf294
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-12-05 21:19:53 +0100

    webpack 5 polyfill inclusion for building

---
+++ ui/config-overrides.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+
+module.exports = function override (config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    path: require.resolve('path-browserify'),
+    os: require.resolve('os-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    process: require.resolve('process/browser.js'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/')
+  }
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ])
+
+  return config
+}
